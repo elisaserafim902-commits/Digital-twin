4a27c62ad173f719a6981b8697f76b5ca99e74db
Elisa Serafim <elisaserafim902@gmail.com>
2026-03-31 10:12:00 -0300

    fix layout remove nextauth

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,18 +1,12 @@
-"use client"
-
-import { SessionProvider } from "next-auth/react"
-
 export default function RootLayout({
 children,
 }: {
 children: React.ReactNode
 }) {
 return (
-<html>
+<html lang="pt-br">
 <body>
-<SessionProvider>
 {children}
-</SessionProvider>
 </body>
 </html>
 )
