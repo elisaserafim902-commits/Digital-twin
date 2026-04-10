82893a0961844e736b1baba2c9fc411c5e703f92
Elisa Serafim <elisaserafim902@gmail.com>
2026-04-10 11:11:55 -0300

    fix: tsconfig alias corrected

--- app/api/auth/[...nextauth]/route.ts
+++ app/api/auth/[...nextauth]/route.ts
@@ -1,6 +1,6 @@
 import NextAuth from "next-auth"
 import CredentialsProvider from "next-auth/providers/credentials"
-import { authenticate } from "../../../lib/auth"
+import { authenticate } from "@/lib/auth"
 
 const handler = NextAuth({
 providers: [
