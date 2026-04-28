10e259213d0285040f0e66d77db5903e112d36bc
Elisa Serafim <elisaserafim902@gmail.com>
2026-04-28 11:24:47 -0300

    fix dependencies final

--- lib/auth.ts
+++ lib/auth.ts
@@ -1,6 +1,6 @@
 import CredentialsProvider from "next-auth/providers/ credentials"
-import bcrypt from "bcrypt"
 import { PrismaClient } from "@prisma/client"
+import bcrypt from "bcrypt"
 
 const prisma = new PrismaClient()
 
@@ -8,38 +8,28 @@ export const authOptions = {
 providers: [
 CredentialsProvider({
 name: "Credentials",
-
-credentials:{
-email:{},
-password:{}
+credentials: {
+email: {},
+password: {}
 },
-
-async authorize(credentials:any){
+async authorize(credentials) {
+if (!credentials?.email || !credentials?.password) return null
 
 const user = await prisma.user.findUnique({
-where:{ email: credentials.email }
+where: { email: credentials.email }
 })
 
-if(!user) return null
+if (!user) return null
 
 const valid = await bcrypt.compare(
 credentials.password,
 user.password
 )
 
-if(!valid) return null
+if (!valid) return null
 
-return {
-id: user.id ,
-email:user.email
-}
+return user
 }
 })
-],
-
-session:{
-strategy:"jwt"
-},
-
-secret: process.env.NEXTAUTH_SECRET
+]
 }
